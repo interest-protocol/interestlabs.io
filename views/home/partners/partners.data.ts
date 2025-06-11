16bad79beb81fe4d58b53deeaa64e04a5cbfd078
Antonio Kipanda Cardoso <34079870+KipandaJr@users.noreply.github.com>
2025-06-11 23:33:06 +0100

    :fire: fix: remove some partners on list

--- views/home/partners/partners.data.ts
+++ views/home/partners/partners.data.ts
@@ -2,89 +2,14 @@ import { PartnersImage } from './partners.types';
 
 export const PARTNERS_IMAGES: ReadonlyArray<PartnersImage> = [
   {
+    image: '/img/suiF.png',
+    label: 'Sui Foundation',
+    url: 'https://sui.io/',
+  },
+  {
     image: '/img/movement-labs.webp',
     label: 'Movement Labs',
     url: 'https://movementlabs.xyz/',
-  },
-  {
-    image: '/img/satay.png',
-    label: 'Satay',
-    url: 'https://www.satay.finance/',
-  },
-  {
-    image: '/img/route-x.png',
-    label: 'Route-X',
-    url: 'https://x.com/routexio',
-  },
-  {
-    image: '/img/after-math.png',
-    label: 'After Math',
-    url: 'https://aftermath.finance/',
-  },
-  {
-    image: '/img/move-wiffrens.png',
-    label: 'Move wiffrens',
-    url: 'https://www.movewiffrens.xyz/',
-  },
-  {
-    image: '/img/up-network.webp',
-    label: 'Up Network',
-    url: 'https://www.upnetwork.xyz/',
-  },
-  {
-    image: '/img/nightly.png',
-    label: 'Nightly',
-    url: 'https://nightly.app/',
-  },
-  {
-    image: '/img/mosaic.png',
-    label: 'Mosaic',
-    url: 'https://www.mosaic.io/',
-  },
-  {
-    image: '/img/pyth.png',
-    label: 'Pyth',
-    url: 'https://www.pyth.network/',
-  },
-  {
-    image: '/img/gorilla-moverz.png',
-    label: 'Gorilla Maverz',
-    url: 'https://coinmarketcap.com/currencies/gorilla-token/',
-  },
-  {
-    image: '/img/recrd-v2.png',
-    label: 'Nemo Prootol',
-    url: 'https://recrd.com',
-  },
-  {
-    image: '/img/bucket.png',
-    label: 'Bucket',
-    url: 'https://www.bucketprotocol.io/',
-  },
-  {
-    image: '/img/scallop.png',
-    label: 'Scallop',
-    url: 'https://www.scallop.io/',
-  },
-  {
-    image: '/img/patara.png',
-    label: 'Patara',
-    url: 'https://patara.app/',
-  },
-  {
-    image: '/img/nemo.png',
-    label: 'Nemo Prootol',
-    url: 'https://www.nemoprotocol.com/',
-  },
-  {
-    image: '/img/studio-mirai.png',
-    label: 'Studio Mirai',
-    url: 'https://x.com/_StudioMirai',
-  },
-  {
-    image: '/img/doubleup.webp',
-    label: 'Double Up',
-    url: 'https://www.doubleup.fun/',
   },
   {
     image: '/img/suiplay.png',
@@ -92,13 +17,24 @@ export const PARTNERS_IMAGES: ReadonlyArray<PartnersImage> = [
     url: 'https://www.suiplay0x1.com/',
   },
   {
+    image:
+      'https://app.canopyhub.xyz/static/media/logomark.aeb126e7f9c06ac4fd94c30bf3acf10e.svg',
+    label: 'Canopy',
+    url: 'https://app.canopyhub.xyz/',
+  },
+  {
+    image: '/img/recrd-v2.png',
+    label: 'Nemo Prootol',
+    url: 'https://recrd.com',
+  },
+  {
     image: '/img/nexa.png',
     label: 'Nexa',
     url: 'https://www.insidex.trade/',
   },
   {
-    image: '/img/suiF.png',
-    label: 'Sui Foundation',
-    url: 'https://sui.io/',
+    image: '/img/mosaic.png',
+    label: 'Mosaic',
+    url: 'https://www.mosaic.io/',
   },
 ];
